ac3c434a8726a8fbf5bed2a48749ca166215faa4
Damien Gallagher <damogallagher@users.noreply.github.com>
2020-05-16 18:10:28 +0100

    Test commit

---
+++ src/test/java/com/cloudstarter/cloudstarter/CloudStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudstarter.cloudstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
